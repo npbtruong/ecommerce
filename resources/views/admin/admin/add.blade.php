cf683d6c78d6500f5592b406e72ec14e287ef352
NpbTruong <truong.nguyen@whl.travel>
2024-10-03 16:46:00 +0700

    Your commit message

---
+++ resources/views/admin/admin/add.blade.php
@@ -0,0 +1,71 @@
+@extends('admin.layouts.app')
+
+@section('style')
+@endsection
+
+@section('content')
+
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-12">
+            <h1>Add New Admin</h1>
+          </div>  
+        </div>
+      </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+      <div class="container-fluid">
+        <div class="row">
+        
+        <div class="col-md-12">
+            <!-- general form elements -->
+            <div class="card card-primary">
+              <form action="" method="post">
+                {{ csrf_field() }}
+                <div class="card-body">
+                  <div class="form-group">
+                    <label>Name</label>
+                    <input name="name" type="text" class="form-control" placeholder="Enter Name" value="{{ old('name') }}" required>
+                  </div>
+                  <div class="form-group">
+                    <label>Email</label>
+                    <input name="email" type="email" class="form-control" placeholder="Enter Email" value="{{ old('email') }}" required>
+                    <div style="color: red;">{{ $errors->first('email') }}</div>
+                  </div>
+                  <div class="form-group">
+                    <label>Password</label>
+                    <input name="password" type="password" class="form-control" placeholder="Password" required>
+                  </div>
+                  <div class="form-group">
+                    <label>Status</label>
+                    <select name="status" class="form-control" required>
+                      <option {{ (old('status') == 0) ? 'selected':'' }} value="0">Active</option>
+                      <option {{ (old('status') == 1) ? 'selected':'' }} value="1">InActive</option>
+                    </select>
+                  </div>
+                </div>
+                <!-- /.card-body -->
+
+                <div class="card-footer">
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+              </form>
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>    
+        
+      </div><!-- /.container-fluid -->
+    </section>
+    <!-- /.content -->
+</div>
+
+@endsection
+
+@section('script')
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('admin', [AuthController::class, 'login_admin']);
+Route::post('admin', [AuthController::class, 'auth_login_admin']);
+Route::get('admin/logout',[AuthController::class, 'logout_admin']);
+
+
+Route::get('admin/dashboard', function () {
+    return view('admin.dashboard');
+});
+
+Route::get('admin/admin/list', function () {
+    return view('admin.admin.list');
 });
